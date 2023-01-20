b1d3d5f08be9790ee2703817fd425a3cfbcb5b71
fradop <hugogoransson1@gmail.com>
2023-01-20 16:26:14 +0100

    lite småändringar

--- src/main/java/kafeterian/test.java
+++ src/main/java/kafeterian/test.java
@@ -23,13 +23,27 @@ public class test {
             }
             boolean addSugar = false;
             boolean addMilk = false;
+            String sugar = "Sugar";
+            String milk = "milk";
             String order = "";
+            String extrasugar = "Do you want extra sugar? (y/n) ";
+            String extramilk = "Do you want extra milk? (y/n) ";
             switch (selection) {
                 case 1:
                     order = "Espresso";
                     break;
                 case 2:
                     order = "Americano";
+                    System.out.print(extrasugar);
+                    sugar = input.next();
+                    if (sugar.equals("y")) {
+                        addSugar = true;
+                    }
+                    System.out.print(extramilk);
+                    milk = input.next();
+                    if (milk.equals("y")) {
+                        addMilk = true;
+                    }
                     break;
                 case 3:
                     order = "Cappuccino";
@@ -42,27 +56,57 @@ public class test {
                     break;
                 case 6:
                     order = "Macchiato";
+                    System.out.print(extrasugar);
+                    sugar = input.next();
+                    if (sugar.equals("y")) {
+                        addSugar = true;
+                    }
+                    System.out.print(extramilk);
+                    milk = input.next();
+                    if (milk.equals("y")) {
+                        addMilk = true;
+                    }
                     break;
                 case 7:
                     order = "Irish Coffee";
+                    System.out.print(extrasugar);
+                    sugar = input.next();
+                    if (sugar.equals("y")) {
+                        addSugar = true;
+                    }
+                    System.out.print(extramilk);
+                    milk = input.next();
+                    if (milk.equals("y")) {
+                        addMilk = true;
+                    }
                     break;
                 case 8:
                     order = "French Press";
+                    System.out.print(extrasugar);
+                    sugar = input.next();
+                    if (sugar.equals("y")) {
+                        addSugar = true;
+                    }
+                    System.out.print(extramilk);
+                    milk = input.next();
+                    if (milk.equals("y")) {
+                        addMilk = true;
+                    }
                     break;
                 default:
                     System.out.println("Invalid selection. Please try again.");
                     continue;
             }
-            System.out.print("Do you want extra sugar? (y/n) ");
-            String sugar = input.next();
-            if (sugar.equals("y")) {
-                addSugar = true;
-            }
-            System.out.print("Do you want extra milk? (y/n) ");
-            String milk = input.next();
-            if (milk.equals("y")) {
-                addMilk = true;
-            }
+            // System.out.print("Do you want extra sugar? (y/n) ");
+            // String sugar = input.next();
+            // if (sugar.equals("y")) {
+            //     addSugar = true;
+            // }
+            // System.out.print("Do you want extra milk? (y/n) ");
+            // String milk = input.next();
+            // if (milk.equals("y")) {
+            //     addMilk = true;
+            // }
             if (addSugar) {
                 order += " with extra sugar";
             }
